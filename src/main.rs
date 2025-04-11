9b6dbd7acda158d88c72e674e333228486e3b557
Oriza <ahmadoriza@gmail.com>
2025-04-11 17:29:38 +0700

    Prepare server, create table, listen incoming

--- src/main.rs
+++ src/main.rs
@@ -15,5 +15,20 @@ struct NewTodo {
 }
 
 fn main() -> Result<()> {
-   
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    println!("Server berjalan di http://localhost:8080");
+
+    let db = Connection::open("todos.db")?;
+    db.execute("CREATE TABLE IF NOT EXISTS todos (id INTEGER PRIMARY KEY, title TEXT);", [])?;
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle_connection(stream, &db)?;
+    }
+
+    Ok(())
+}
+
+fn handle_connection(mut stream: TcpStream, db: &Connection) -> Result<()> {
+
 }
